06e235a84a473fd5067e33c17e82266a85422d0c
peterlavin <peterlavin01@gmail.com>
2017-10-02 14:13:14 +0100

    Some comments/doc comments added

--- LearnR2Rml/src/main/java/com/blockty/R2rmlToXML/RunImportMapping.java
+++ LearnR2Rml/src/main/java/com/blockty/R2rmlToXML/RunImportMapping.java
@@ -13,29 +13,27 @@ import xmlutilities.EmptyXMLDocument;
 import xmlutilities.PrettyPrintXML;
 
 public class RunImportMapping {
-
-	// private static String CONSTRUCTSMAPS = "PREFIX rr:
-	// <http://www.w3.org/ns/r2rml#> CONSTRUCT { ?x rr:subjectMap [ rr:constant
-	// ?y ]. } WHERE { ?x rr:subject ?y. }";
-	// private static String CONSTRUCTOMAPS = "PREFIX rr:
-	// <http://www.w3.org/ns/r2rml#> CONSTRUCT { ?x rr:objectMap [ rr:constant
-	// ?y ]. } WHERE { ?x rr:object ?y. }";
-	// private static String CONSTRUCTPMAPS = "PREFIX rr:
-	// <http://www.w3.org/ns/r2rml#> CONSTRUCT { ?x rr:predicateMap [
-	// rr:constant ?y ]. } WHERE { ?x rr:predicate ?y. }";
-	// private static String CONSTRUCTGMAPS = "PREFIX rr:
-	// <http://www.w3.org/ns/r2rml#> CONSTRUCT { ?x rr:graphMap [ rr:constant ?y
-	// ]. } WHERE { ?x rr:graph ?y. }";
+	/**
+	 * 
+	 * Class reads a R2RML linked-data mapping file (.ttl)
+	 * and outputs an XML file in the format required by
+	 * (Google's) Blockty for graphical display/editing.
+	 * The XML produced is of the same schema as that
+	 * underlying Blockty.
+	 * 
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 
 		String mappingFile = "";
-		mappingFile = "samples/example_2.ttl";
-		mappingFile = "samples/cso_has_sql.ttl";
-		mappingFile = "samples/2_tm_with_class_iri.ttl";
-		mappingFile = "samples/3_tms_class_iri_none.ttl";
-		mappingFile = "samples/4_tms_class_iri_blank_none.ttl";
-		mappingFile = "samples/various_testing.ttl";
+		/*
+		 * mappingFile = "samples/example_2.ttl"; mappingFile =
+		 * "samples/cso_has_sql.ttl"; mappingFile = "samples/2_tm_with_class_iri.ttl";
+		 * mappingFile = "samples/3_tms_class_iri_none.ttl"; mappingFile =
+		 * "samples/4_tms_class_iri_blank_none.ttl"; mappingFile =
+		 * "samples/various_testing.ttl";
+		 */
 		mappingFile = "samples/various_testing.ttl";
 
 		
@@ -45,7 +43,7 @@ public class RunImportMapping {
 		 */
 		R2RMLMapping map = R2RMLMappingFactory.createR2RMLMapping(mappingFile, null);
 
-		System.out.println("\n========================================================\n\n");
+		System.out.println("\n==========================\n\n");
 
 		/*
 		 * Get a map of all the triplesmaps in the model
